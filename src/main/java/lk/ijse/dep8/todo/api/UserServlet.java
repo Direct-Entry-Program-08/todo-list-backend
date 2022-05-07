8f060789033595ed5b49091f38a076d1d5069d4d
Thilina-Upendra <upendravass1996@gmail.com>
2022-05-07 19:30:29 +0530

    Finalize the user save and update

--- src/main/java/lk/ijse/dep8/todo/api/UserServlet.java
+++ src/main/java/lk/ijse/dep8/todo/api/UserServlet.java
@@ -27,118 +27,12 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        /*Add content type check*/
-        if(request.getContentType() == null || !request.getContentType().toLowerCase().startsWith("application/json")){
-            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Wrong content type|");
-            return;
-        }
-
-        /*Create validation to the json object*/
-        try{
-            Jsonb jsonb = JsonbBuilder.create();
-            UserDTO user = jsonb.fromJson(request.getReader(), UserDTO.class);
-
-            /*Validate the user details*/
-            if(user.getEmail() == null || !user.getEmail().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
-                throw new ValidationException("Invalid Email");
-            }else if(user.getName() == null || !user.getName().matches("[A-Za-z ]+")){
-                throw new ValidationException("Invalid Name");
-            }else if(user.getPassword() == null || !user.getPassword().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$")){
-                /*
-                Password must contain at least one digit [0-9].
-                Password must contain at least one lowercase Latin character [a-z].
-                Password must contain at least one uppercase Latin character [A-Z].
-                Password must contain at least one special character like ! @ # & ( ).
-                Password must contain a length of at least 8 characters and a maximum of 20 characters.
-                 */
-                throw new ValidationException("Invalid password");
-            }
-
-            try(Connection connection = pool.getConnection()){
-
-                PreparedStatement stm = null;
-                stm = connection.prepareStatement("SELECT * FROM User WHERE email = ?");
-                stm.setString(1, user.getEmail());
-                ResultSet results = stm.executeQuery();
-                if(results.next()){
-                    /*There is a duplicate user*/
-                    response.sendError(HttpServletResponse.SC_CONFLICT, "This user is already in the system");
-                    return;
-                }
-
-                stm = connection.prepareStatement("INSERT INTO User (email, name, password) VALUES (?,?,?)");
-                stm.setString(1, user.getEmail());
-                stm.setString(2, user.getName());
-                stm.setString(3, user.getPassword());
-
-                int savedResult = stm.executeUpdate();
-                if(savedResult != 1){
-                    response.sendError(HttpServletResponse.SC_NO_CONTENT, "Failed to save user to the DB");
-                    return;
-                }
-                response.sendError(HttpServletResponse.SC_CREATED, "Saved user to the DB");
-            }
-        }catch (ValidationException | JsonbException e){
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, (e instanceof JsonbBuilder)?"Invalid":e.getMessage());
-        }catch (Throwable t){
-            t.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
-
+        doSaveOrUpdate(request, response);
     }
 
     @Override
     protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-        /*Check the url of the request and validate the email of the user*/
-        if(request.getPathInfo() == null || !request.getPathInfo().matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")){
-            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid user mail");
-            return;
-        }else if(request.getContentType() == null || !request.getContentType().toLowerCase().startsWith("application/json")){
-            response.sendError(HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE, "Invalid content type");
-            return;
-        }
-
-        /*Add details to the object and validate the request details*/
-        try{
-            Jsonb jsonb = JsonbBuilder.create();
-            UserDTO user = jsonb.fromJson(request.getReader(), UserDTO.class);
-            user.setEmail(request.getPathInfo().substring(1));
-
-            if(user.getName() == null || !user.getName().matches("[A-Za-z ]+")){
-                throw new ValidationException("Invalid user name");
-            }else if(user.getPassword() == null || !user.getPassword().matches("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$")){
-                throw new ValidationException("Invalid user password");
-            }
-
-            try(Connection connection = pool.getConnection()){
-
-                /*First check this user in the DB or not*/
-                PreparedStatement stm = connection.prepareStatement("SELECT * FROM User WHERE email=?");
-                stm.setString(1, user.getEmail());
-                ResultSet results = stm.executeQuery();
-                if(results.next()){
-                   /*TODO: Have to update the user*/
-                    stm = connection.prepareStatement("UPDATE User SET name=?, password=? WHERE email=?");
-                    stm.setString(1, user.getName());
-                    stm.setString(2, user.getPassword());
-                    stm.setString(3, user.getEmail());
-                    int updateResults = stm.executeUpdate();
-                    if(updateResults != 1){
-                        response.sendError(HttpServletResponse.SC_CONFLICT, "Failed to update user");
-                        return;
-                    }
-                    response.sendError(HttpServletResponse.SC_CREATED);
-                }else{
-                    response.sendError(HttpServletResponse.SC_NOT_FOUND, "The user is not found in the DB");
-                }
-            }
-        }catch (JsonbException | ValidationException e){
-            response.sendError(HttpServletResponse.SC_BAD_REQUEST, (e instanceof JsonbBuilder) ? "Invalid json":e.getMessage());
-        }catch (Throwable t){
-            t.printStackTrace();
-            response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
+        doSaveOrUpdate(request, response);
     }
 
 
@@ -153,8 +47,9 @@ public class UserServlet extends HttpServlet {
         String method = request.getMethod();
         String pathInfo = request.getPathInfo();
 
-        if(method.equals("POST") && !(pathInfo.equalsIgnoreCase("/users") || pathInfo.equalsIgnoreCase("/users/"))){
-            response.sendError(HttpServletResponse.SC_NOT_FOUND);
+        if(method.equals("POST") && !(request.getServletPath().equalsIgnoreCase("/users") || request.getServletPath().equalsIgnoreCase("/users/"))){
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Invalid url pattern");
+            return;
         }else if(method.equals("PUT") && !(pathInfo != null && pathInfo.substring(1).matches("(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])"))){
             response.sendError(HttpServletResponse.SC_NOT_FOUND, "User not found in the DB");
             return;
@@ -193,12 +88,31 @@ public class UserServlet extends HttpServlet {
                 /*Save or Update logic*/
                 if(results.next()){
                     if(method.equals("POST")){
-                        /*TODO: Saved user already here*/
+
+                        response.sendError(HttpServletResponse.SC_CONFLICT, "This user already saved in the DB");
                     }else{
-                        /*TODO: Update the user*/
+
+                        stm = connection.prepareStatement("UPDATE User SET name=?, password=? WHERE email=?");
+                        stm.setString(1, user.getName());
+                        stm.setString(2, user.getPassword());
+                        stm.setString(3, user.getEmail());
+                        int updatedResults = stm.executeUpdate();
+                        if(updatedResults != 1){
+                            throw new RuntimeException("Failed to Update user");
+                        }
+                        response.setStatus(HttpServletResponse.SC_CREATED);
                     }
                 }else{
-                    /*TODO: There is no user because of that have to save the user*/
+
+                    stm = connection.prepareStatement("INSERT INTO User (email, name, password) VALUES (?,?,?)");
+                    stm.setString(1, user.getEmail());
+                    stm.setString(2, user.getName());
+                    stm.setString(3, user.getPassword());
+                    int savedResults = stm.executeUpdate();
+                    if(savedResults != 1){
+                        throw new RuntimeException("Failed to Save user");
+                    }
+                    response.setStatus(HttpServletResponse.SC_CREATED);
                 }
             }
             
